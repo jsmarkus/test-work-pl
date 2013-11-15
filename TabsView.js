e3e1eea7037a6bdd5dcf165ecd73c623ed4ff2c9
Mark <me@jsman.ru>
2013-11-15 16:19:23 +0200

    implementation activeTab, addTab, removeTab

--- TabsView.js
+++ TabsView.js
@@ -14,6 +14,11 @@
 
     function TabsView() {
         this.assets = {};
+        this._tabs = [];
+        this._tabsById = {};
+
+        this._titleById = {};
+        this._paneById = {};
     }
 
     var proto = TabsView.prototype;
@@ -25,8 +30,6 @@
         var asPanes = assets.panes = this._createPanesContainer();
         asMain.appendChild(asTitles);
         asMain.appendChild(asPanes);
-        this._tabs = [];
-        this._tabsById = {};
     };
 
     proto.addTab = function(id, title, content) {
@@ -47,23 +50,33 @@
         //TODO: IE ployfill
         this._tabs.splice(index, 1);
         delete this._tabsById[id];
-        this._applyRemoveTab(index);
+        this._applyRemoveTab(id);
     };
 
-    proto.activeTab = function() {
-
+    proto.activeTab = function(id) {
+        //TODO: get
+        this._activeTabId = id;
+        this._applyActiveTab(id);
     };
 
     proto.tabTitle = function() {
 
     };
 
-    proto._createTabTitle = function() {
-
+    proto._createTabTitle = function(tab) {
+        return domElement('li', null, [
+            domElement('a', {
+                'href' : 'javascript:void(0)',
+                'data-tab-id' : tab.id,
+                $text : tab.title
+            })
+        ]);
     };
 
-    proto._createTabPane = function() {
-
+    proto._createTabPane = function(tab) {
+        return domElement('div', {
+            $html : tab.content //TODO: normalize to allow use string as well as HTMLElement
+        });
     };
 
     proto._createMainContainer = function() {
@@ -85,22 +98,61 @@
     };
 
     proto._applyAddTab = function(tab) {
+        var title = this._createTabTitle(tab);
+        var pane = this._createTabPane(tab);
+        this.assets.titles.appendChild(title);
+        this.assets.panes.appendChild(pane);
 
+        this._titleById[tab.id] = title;
+        this._paneById[tab.id] = pane;
     };
 
-    proto._applyRemoveTab = function(index) {
+    proto._applyRemoveTab = function(id) {
+        //TODO: works?
+        this.assets.titles.removeChild(this._titleById[id]);
+        this.assets.panes.removeChild(this._paneById[id]);
 
+        delete this._titleById[id];
+        delete this._paneById[id];
+    };
+    
+    proto._applyActiveTab = function(id) {
+        var title = this._titleById[id];
+        title.setAttribute('class', 'active');
     };
 
-    function domElement(tag, attrs) {
-        var el = document.createElement(tag);
+    proto._getTitleElement = function (id) {
+        return this._titleById[id];
+    };
+
+    proto._getPaneElement = function (id) {
+        return this._paneById[id];
+    };
+
+    function domElement(tag, attrs, children) {
         attrs = attrs || {};
+        children = children || {};
+        var el = document.createElement(tag);
         for (var attrName in attrs) {
             if (!attrs.hasOwnProperty(attrName)) {
                 continue;
             }
-            el.setAttribute(attrName, attrs[attrName]);
+            var value = attrs[attrName];
+            if(attrName === '$text') {
+                el.innerText = value;
+                continue;
+            }
+            if(attrName === '$html') {
+                el.innerHTML = value;
+                continue;
+            }
+            el.setAttribute(attrName, value);
         }
+
+        for (var i = 0; i < children.length; i++) {
+            el.appendChild(children[i]);
+        }
+
         return el;
     }
 
--- demo.js
+++ demo.js
@@ -6,3 +6,14 @@ var tabs = new TabsView();
 tabs.render();
 console.log(tabs);
 document.body.appendChild(tabs.assets.main);
+
+tabs.addTab('one', 'One', 'Lorem ipsum');
+tabs.addTab('two', 'Two', 'Hello world');
+tabs.addTab('three', 'Three', 'Foo bar');
+tabs.addTab('four', 'Four', 'Foo bar');
+tabs.addTab('five', 'Five', 'Foo bar');
+tabs.addTab('six', 'Six', 'Foo bar');
+
+tabs.removeTab('two');
+
+tabs.activeTab('three');
